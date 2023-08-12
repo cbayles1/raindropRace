17fbc05960cabe62771442a7a12e2ee1ae34bb51
Caleb <clbayles@hotmail.com>
2023-08-12 18:57:17 -0500

    added db file

---
+++ server/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `Users`(
+    `userID` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `raindropID` INT UNSIGNED NULL AUTO_INCREMENT,
+    `wins` INT UNSIGNED NOT NULL,
+    `displayName` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `Raindrops`(
+    `raindropID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `votes` BIGINT UNSIGNED NOT NULL,
+    `isWinner` TINYINT(1) NOT NULL
+);
+ALTER TABLE
+    `Users` ADD CONSTRAINT `users_raindropid_foreign` FOREIGN KEY(`raindropID`) REFERENCES `Raindrops`(`raindropID`);
