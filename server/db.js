332df55d812984d5ed401b8bf29b96e7bf44f954
Caleb <clbayles@hotmail.com>
2023-08-13 18:14:34 -0500

    added users table to interface

--- server/db.js
+++ server/db.js
@@ -5,9 +5,10 @@ dotenv.config();
 const { DATABASE_URL } = process.env;
 const sql = postgres(DATABASE_URL, { ssl: 'require' });
 
+// CREATE TABLES
 async function createRaindropTable() {
     await sql`
-        CREATE TABLE raindrops(
+        CREATE TABLE public.raindrops(
             "raindrop_id" SERIAL PRIMARY KEY,
             "votes" INT NOT NULL,
             "is_winner" BOOL NOT NULL,
@@ -15,7 +16,26 @@ async function createRaindropTable() {
         );
     `;
 }
+async function createUsersTable() {
+    await sql`
+        CREATE TABLE public.users(
+        "user_id" SERIAL PRIMARY KEY,
+        "raindrop_id" INT NULL,
+        "wins" INT NOT NULL,
+        "display_name" VARCHAR(20) NOT NULL UNIQUE
+      );
+    `;
+}
 
+// DROP TABLES
+async function dropRaindropTable() {
+    await sql`DROP TABLE public.raindrops`;
+}
+async function dropUsersTable() {
+    await sql`DROP TABLE public.users`;
+}
+
+// ADD ENTRIES
 async function addRaindrop() {
     await sql`
         INSERT INTO public.raindrops
@@ -24,5 +44,15 @@ async function addRaindrop() {
     `;
 }
 
-//createRaindropTable();
-addRaindrop();
+async function addUser(displayName) {
+    await sql`
+        INSERT INTO public.users
+        (display_name, wins)
+        VALUES (${displayName}, ${0});
+    `;
+}
+
+
+["Bob", "Alice", "Tom", "Dick", "Harry"].map((name) => {
+    addUser(name);
+});
