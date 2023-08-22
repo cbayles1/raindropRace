5fc4ecd864ac423961de2b864fe54c3fc46d6a9f
Caleb <clbayles@hotmail.com>
2023-08-21 21:18:18 -0500

    pulled from api into webpage

--- app/page.js
+++ app/page.js
@@ -1,94 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+//import Image from 'next/image'
+//import styles from './page.module.css'
 
-export default function Home() {
+async function getTurtles() {
+  const res = await fetch("http://localhost:3000/api/getAllTurtlesDataNoVel/");
+  return await res.json();
+}
+
+function Turtle({turtleData}) {
+  let winnerDisplay = "false";
+  if (turtleData.is_winner) {
+    winnerDisplay = "true";
+  }
+
+  return (<div>
+    <h3>{turtleData.name}</h3>
+    <ul>
+      <li>Votes: {turtleData.votes}</li>
+      <li>Position: {turtleData.position}</li>
+      <li>Is the Winner: {winnerDisplay}</li>
+    </ul>
+  </div>);
+}
+
+export default async function Home() {
+  const turtles = await getTurtles();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div>
+      {
+        turtles.map((turtle) => {
+          return <Turtle turtleData={turtle}></Turtle>;
+        })
+      }
       </div>
     </main>
   )
