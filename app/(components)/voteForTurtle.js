3cf88edc1af2671c93e25d71bbcb4e2538d0db38
Caleb <clbayles@hotmail.com>
2024-05-14 12:42:21 -0500

    something in the json body might work

--- app/(components)/voteForTurtle.js
+++ app/(components)/voteForTurtle.js
@@ -3,13 +3,17 @@ import {getUserSession} from '../lib/session';
 
 export async function voteForTurtle(turtleId) {
     const user = await getUserSession();
-    const res = await fetch(`${process.env.DOMAIN_NAME}/api/vote/`, {
+    console.log(user.id);
+    console.log(turtleId);
+    const res = await fetch(`${process.env.MY_DOMAIN_NAME}/api/vote/`, {
         method: 'POST',
         mode: 'cors',
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
+            'Content-type': 'application/json; charset=UTF-8'
         },
-        body: JSON.stringify({'userId': user.id, 'turtleId': turtleId})
+        body: JSON.stringify({
+            'userId': user.id, 
+            'turtleId': turtleId
+        })
     });
 }
