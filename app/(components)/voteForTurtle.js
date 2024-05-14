b7b1d7492f59348969cae037ef2a877b2167b34e
Caleb <clbayles@hotmail.com>
2024-05-14 12:54:01 -0500

    removed print log

--- app/(components)/voteForTurtle.js
+++ app/(components)/voteForTurtle.js
@@ -3,7 +3,6 @@ import {getUserSession} from '../lib/session';
 
 export async function voteForTurtle(turtleId) {
     const user = await getUserSession();
-    console.log(process.env.DOMAIN_NAME);
     const res = await fetch(`${process.env.DOMAIN_NAME}/api/vote/`, {
         method: 'POST',
         mode: 'cors',
