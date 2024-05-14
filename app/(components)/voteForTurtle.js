76b24898d3dd17a339e04b50b5e6f3b9f3a600e1
Caleb <clbayles@hotmail.com>
2024-05-14 12:11:53 -0500

    attempting to fix prod voting issue

--- app/(components)/voteForTurtle.js
+++ app/(components)/voteForTurtle.js
@@ -5,6 +5,7 @@ export async function voteForTurtle(turtleId) {
     const user = await getUserSession();
     const res = await fetch(`${process.env.DOMAIN_NAME}/api/vote/`, {
         method: 'POST',
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({'userId': user.id, 'turtleId': turtleId})
     });
 }
