dd29493eb6e6abc2cf70ce52ce2d7ebaa7e86dc2
Caleb <clbayles@hotmail.com>
2024-05-15 12:06:18 -0500

    fixed hover and disabled states (I think)

--- app/(components)/VoteOptions.js
+++ app/(components)/VoteOptions.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function VoteOptions({turtles, turtlePicked}) {
     const [userVote, setUserVote] = useState(turtlePicked);
-    const [hoverColor, setHoverColor] = useState('nero');
+    //const [hoverColor, setHoverColor] = useState('nero');
 
     return (
         <div id="voteOptions" className='grid grid-flow-col gap-4 mr-4 mt-4'>
@@ -18,40 +18,33 @@ export default function VoteOptions({turtles, turtlePicked}) {
     );
 
     function VoteOption({turtle}) {
-        const [bgColor, setBgColor] = useState("alpine");
+        const [bgColor, setBgColor] = useState('alpine');
+        const [hoverColor, setHoverColor] = useState('nero');
+        const [isDisabled, setDisabled] = useState(false);
 
         const turtleImg = <Image className='mx-4' alt={turtle.name} src={`/turtles/${turtle.name.toLowerCase()}.png`} width={60} height={60}></Image>;
-
-        function checkButtonColors(userVote) {
-            if (userVote) {
-                setHoverColor("alpine");
-                if (turtle.id == userVote) {
-                    setBgColor("nero");
-                } else {
-                    setBgColor("alpine");
-                }
-            } else {
-                setHoverColor("nero");
-                if (userVote == turtle.id) {
-                    setBgColor("nero");
-                } else {
-                    setBgColor("alpine");
-                }
-            }
-        }
 
         if (turtle.position >= 50 && !userVote) { // if any turtle is past 50 and user hasn't voted, user cannot vote on any turtle
             setUserVote(-1);
         }
 
         useEffect(() => {
-            checkButtonColors(userVote);
+            if (userVote) {
+                setHoverColor('alpine');
+                setDisabled(true);
+                if (turtle.id == userVote) {
+                    setBgColor('nero');
+                } else {
+                    setBgColor('alpine');
+                }
+            } else {
+                setHoverColor('nero');
+                setDisabled(false);
+                setBgColor('alpine');
+            }
         }, [userVote]);
-        useEffect(() => {
-            checkButtonColors(userVote);
-        }, []);
 
-        return (<button value={turtle.id.toString()} onClick={handleButtonClick} 
+        return (<button value={turtle.id.toString()} onClick={handleButtonClick} disabled={isDisabled}
           className={`grid grid-cols-1 py-3 rounded-md bg-${bgColor} w-full h-full place-items-center space-y-4 min-w-16 hover:bg-${hoverColor}`}>
             {turtleImg}
             <label htmlFor="turtle" className="text-oat font-mono">{turtle.name}</label>
@@ -59,7 +52,7 @@ export default function VoteOptions({turtles, turtlePicked}) {
 
         async function handleButtonClick(event) {
             if (userVote) { // if a turtle was already picked, don't allow any more votes
-                setHoverColor("alpine");
+                //setHoverColor('alpine');
                 return;
             }
             const turtleIdStr = event.currentTarget.value;
