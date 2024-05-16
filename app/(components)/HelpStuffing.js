255a04eb2787820a6444376586f4838d8b79928f
Caleb <clbayles@hotmail.com>
2024-05-15 22:09:03 -0500

    updated help dialog

--- app/(components)/HelpStuffing.js
+++ app/(components)/HelpStuffing.js
@@ -1,9 +1,16 @@
-export default function LoginBox() {
+export default function LoginBox({showDesc=true}) {
+    let desc = <></>;
+    if (showDesc) {
+        desc = <p id="desc">Welcome! Once logged in, you are free to place your bets on which turtle will win the race!</p>
+    }
     return (
         <div className='flex flex-col justify-self-center text-oat text-center text-sm md:text-md lg:text-lg space-y-2'>
-            <p id="refresh">These turtles do not like you watching them move. When users refresh the page, they keep going.</p>
-            <p id="noTakebacks">Once you have voted on a turtle, you cannot change your vote until the next race.</p>
-            <p id="fifty">Once any turtle has made it 50 inches, you cannot vote for the remainder of the race.</p>
+            {desc}
+            <p id="noTakebacks">Careful! Once you have voted on a turtle, you cannot change your vote until next race.</p>
+            <p id="fifty">And once any turtle has made it 50 inches, you cannot vote for the remainder of the race.</p>
+            <p id="refresh">These turtles don't like being watched while they move. When users refresh the page, the turtles keep going.</p>
+            <p id="login">Apologies about the login page; it's safe I promise! I just can't make the warning disappear.</p>
+            <p id="login2">I also can't figure out how to remove the second login button yet. Working on it.</p>
         </div>
     );
 }
--- app/help/page.js
+++ app/help/page.js
@@ -2,6 +2,6 @@ import HelpStuffing from '../(components)/HelpStuffing';
 
 export default async function Help() {
     return (<div className='m-10'>
-        <HelpStuffing></HelpStuffing>
+        <HelpStuffing showDesc={false}></HelpStuffing>
     </div>);
 }
